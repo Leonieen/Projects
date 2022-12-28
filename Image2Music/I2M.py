965242dd54a74742779a2b1b33f815734ef4cf01
Leonieen <48885814+Leonieen@users.noreply.github.com>
2022-12-28 18:57:18 +0100

    Update I2M.py

--- Image2Music/I2M.py
+++ Image2Music/I2M.py
@@ -6,16 +6,10 @@ import pandas as pd
 import numpy as np
 import cv2
 import random
-from pedalboard import Pedalboard, Chorus, Reverb, Gain, LadderFilter, Phaser, Delay, PitchShift, Distortion
 from pedalboard.io import AudioFile
 from PIL import Image
 from scipy.io import wavfile
 import librosa
-import glob
-import leafmap
-import leafmap.leafmap as leafmap
-import leafmap.foliumap as leafmap
-
 
 # This function generates frequencies in Hertz from notes
 def get_piano_notes():
